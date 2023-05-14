e9165874775b3a01719c47a7985d97c8821510de
LMSerhii <Leonovserhii89@gmail.com>
2023-05-14 10:05:50 +0300

    debug

--- live/urls.py
+++ live/urls.py
@@ -3,7 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('result/', result, name='result'),
-    path('second_variable/', second, name='second'),
-    path('second_variable/result', result_2, name='result_2')
+    path('result/', result, name='result')
 ]
--- live/views.py
+++ live/views.py
@@ -23,7 +23,7 @@ def result(request):
 
         month = num_sum(res[1])  # первый сверху
         day = num_sum(res[0])  # первый слева
-        year = num_sum(res[1])  # первый справа
+        year = num_sum(res[2])  # первый справа
         f_1 = num_sum((day + month + year))  # Первый снизу
 
         f_2 = num_sum((f_1 * 2))  # центр
@@ -105,100 +105,3 @@ def result(request):
             'title': 'Результат',
         }
     return render(request, 'result.html', context=context)
-
-
-def second(request):
-    context = {
-        'title': 'Второй результат'
-    }
-    return render(request, 'second_variable.html', context=context)
-
-
-def result_2(request):
-    if request.method == 'GET':
-        birthday = request.GET.get('birth')
-        res = birthday.split('-')
-        month = num_sum(res[1])  # первый сверху
-        day = num_sum(res[2])  # первый слева
-        year = num_sum(res[0])  # первый справа
-        f_1 = num_sum((day + month + year))  # Первый снизу
-
-        f_2 = num_sum((f_1 * 2))  # центр
-        f_3 = num_sum((day + f_2))  # третий слева
-        f_4 = num_sum((day + f_3))  # второй слева
-        f_5 = num_sum((month + f_2))  # третий сверху
-        f_6 = num_sum((month + f_5))  # второй сверху
-        f_7 = num_sum((year + f_2))  # третий справа
-        f_8 = num_sum((year + f_7))  # второй справа
-        f_9 = num_sum((f_1 + f_2))  # третий снизу
-        f_10 = num_sum((f_1 + f_2))  # второй снизу
-        f_11 = num_sum((f_7 + f_9))  # четвертый правый-низ
-        f_12 = num_sum((f_11 + f_9))  # love
-        f_13 = num_sum((f_7 + f_11))  # money
-
-        f_14 = num_sum((day + f_1))  # первый левый-низ
-        f_18 = num_sum((f_2 + f_14))  # третий левый-низ
-        f_19 = num_sum((f_18 + f_14))  # второй левый-низ
-
-        f_15 = num_sum((year + f_1))  # первый правый-низ
-        f_20 = num_sum((f_2 + f_15))  # третий правый-низ
-        f_21 = num_sum((f_20 + f_15))  # второй правый-низ
-
-        f_16 = num_sum((year + month))  # первый правый-верх
-        f_22 = num_sum((f_2 + f_16))  # третий правый-верх
-        f_23 = num_sum((f_22 + f_16))  # второй правый-верх
-
-        f_17 = num_sum((day + month))  # первый левый-верх
-        f_24 = num_sum((f_2 + f_17))  # третий левый-верх
-        f_25 = num_sum((f_24 + f_17))  # второй левый-верх
-
-        #####################################################
-
-        s_1 = num_sum((f_24 + f_22 + f_20 + f_18))  # 4 низ
-        s_2 = num_sum((f_25 + f_23 + f_21 + f_19))  # 3 низ
-        s_3 = num_sum((f_17 + f_16 + f_15 + f_14))  # 2 низ
-        s_4 = num_sum((f_24 + f_20))  # правый низ от центра
-        s_5 = num_sum((f_22 + f_18))  # левый низ от центра
-
-        context = {
-            'title': 'Результат',
-            'birthday': birthday,
-            'year': year,
-            'day': day,
-            'month': month,
-            'f_1': f_1,
-            'f_2': f_2,
-            'f_3': f_3,
-            'f_4': f_4,
-            'f_5': f_5,
-            'f_6': f_6,
-            'f_7': f_7,
-            'f_8': f_8,
-            'f_9': f_9,
-            'f_10': f_10,
-            'f_11': f_11,
-            'f_12': f_12,
-            'f_13': f_13,
-            'f_14': f_14,
-            'f_15': f_15,
-            'f_16': f_16,
-            'f_17': f_17,
-            'f_18': f_18,
-            'f_19': f_19,
-            'f_20': f_20,
-            'f_21': f_21,
-            'f_22': f_22,
-            'f_23': f_23,
-            'f_24': f_24,
-            'f_25': f_25,
-            's_1': s_1,
-            's_2': s_2,
-            's_3': s_3,
-            's_4': s_4,
-            's_5': s_5,
-        }
-    else:
-        context = {
-            'title': 'Результат',
-        }
-    return render(request, 'result_2.html', context=context)
